c7ae33165b3a64a30ac23ea6e5a0eb69685b63e4
Chen.Zhidong <njutczd@gmail.com>
2016-05-27 11:27:47 +0800

    IP定位不打开浏览器

--- ip.go
+++ ip.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/astaxie/beego/httplib"
-	"github.com/cosiner/gohper/regexp"
 	"github.com/toqueteos/webbrowser"
 	"strings"
 )
@@ -16,6 +15,18 @@ func ip(ipaddr string) {
 	ipip_sub("taobao", ipaddr)
 	ipip_sub("sina", ipaddr)
 	ipip_sub("baidu", ipaddr)
+	//ipbaidu(ipaddr)
+}
+
+type iptimezone struct {
+	CityCode    int64    `json:"city_code"`
+	Continent   string   `json:"continent"`
+	CountryCode string   `json:"country_code"`
+	En          []string `json:"en"`
+	Latitude    float64  `json:"latitude,string"`
+	Longitude   float64  `json:"longitude,string"`
+	Timezone    string   `json:"timezone"`
+	Timezone2   string   `json:"timezone2"`
 }
 
 func ipip(ipaddr string) {
@@ -42,34 +53,24 @@ func ipip(ipaddr string) {
 				myself := page.Find("#myself").Text()
 				fmt.Println(strings.TrimSpace(myself))
 
-				reg := regexp.MustCompile(`var ip_data = ({(.*)})?`)
-				ipdata := reg.FindString(page.Text())
-				if len(ipdata) > 0 {
-					jstring := ipdata[strings.Index(ipdata, "{") : strings.Index(ipdata, "}")+1]
-					type iptimezone struct {
-						CityCode    int64    `json:"city_code"`
-						Continent   string   `json:"continent"`
-						CountryCode string   `json:"country_code"`
-						En          []string `json:"en"`
-						Latitude    float64  `json:"latitude,string"`
-						Longitude   float64  `json:"longitude,string"`
-						Timezone    string   `json:"timezone"`
-						Timezone2   string   `json:"timezone2"`
-					}
-					var timezone iptimezone
-					err := json.Unmarshal([]byte(jstring), &timezone)
-					if err != nil {
-						fmt.Println(err)
-					} else {
-						bytes, err := json.MarshalIndent(timezone, "", "    ")
-						if err != nil {
-							fmt.Println(err)
-						} else {
-							fmt.Println(string(bytes))
-							view(timezone.Latitude, timezone.Longitude, ipaddr)
-						}
-					}
-				}
+				//reg := regexp.MustCompile(`var ip_data = ({(.*)})?`)
+				//ipdata := reg.FindString(page.Text())
+				//if len(ipdata) > 0 {
+				//	jstring := ipdata[strings.Index(ipdata, "{") : strings.Index(ipdata, "}")+1]
+				//
+				//	var timezone iptimezone
+				//	err := json.Unmarshal([]byte(jstring), &timezone)
+				//	if err != nil {
+				//		fmt.Println(err)
+				//	} else {
+				//		bytes, err := json.MarshalIndent(timezone, "", "    ")
+				//		if err != nil {
+				//			fmt.Println(err)
+				//		} else {
+				//			fmt.Println(string(bytes))
+				//		}
+				//	}
+				//}
 
 				//chinacode := page.Find(".china_code").Text()
 				//fmt.Println(strings.TrimSpace(chinacode))
@@ -123,9 +124,58 @@ func ipip_sub(site, ipaddr string) {
 	}
 }
 
+type iplocationbaidu struct {
+	Address string `json:"address"`
+	Content struct {
+		Address       string `json:"address"`
+		AddressDetail struct {
+			City         string `json:"city"`
+			CityCode     int64  `json:"city_code"`
+			District     string `json:"district"`
+			Province     string `json:"province"`
+			Street       string `json:"street"`
+			StreetNumber string `json:"street_number"`
+		} `json:"address_detail"`
+		Point struct {
+			X float64 `json:"x,string"`
+			Y float64 `json:"y,string"`
+		} `json:"point"`
+	} `json:"content"`
+	Status int64 `json:"status"`
+}
+
+func ipbaidu(ipaddr string) {
+	fmt.Println("baidu api")
+	ak := "GeWeL6ghCAG7WoBjmxHim0Gp"
+	url := fmt.Sprintf("http://api.map.baidu.com/location/ip?ip=%v&ak=%v&coor=bd09ll", ipaddr, ak)
+	request := httplib.Get(url)
+	resp, err := request.Response()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		if resp.StatusCode != 200 {
+			fmt.Println(resp.Status)
+		} else {
+			var location iplocationbaidu
+			err := request.ToJSON(&location)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				view(location.Content.Point.Y, location.Content.Point.X, ipaddr)
+				bytes, err := json.MarshalIndent(location, "", "    ")
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println(string(bytes))
+				}
+			}
+		}
+	}
+}
+
 func view(lat, log float64, ip string) {
 	if lat > 0 && log > 0 {
-		url := fmt.Sprintf("http://api.map.baidu.com/geocoder?location=%v,%v&coord_type=gcj02&output=html&src=%v", lat, log, ip)
+		url := fmt.Sprintf("http://api.map.baidu.com/geocoder?location=%v,%v&coord_type=bd09ll&output=html", lat, log)
 		webbrowser.Open(url)
 	}
 }
--- ip_test.go
+++ ip_test.go
@@ -5,3 +5,7 @@ import "testing"
 func TestIpip(t *testing.T) {
 	ipip("180.115.100.99")
 }
+
+func TestBaidu(t *testing.T) {
+	ipbaidu("112.82.160.64")
+}
